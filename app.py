6c147aa7efafa7be4c1824ef2522e589ad793c0f
Miguel Silva Gonallves <312750021+miguelwrks@users.noreply.github.com>
2026-09-14 21:00:20 +0000

    feat: codigo com uso do match case e if aninhado ao case para separaçao do conswumo

--- app.py
+++ app.py
@@ -17,3 +17,28 @@
 print('---> Programa Consumo Agua <---')
 
 imovel = str(input('Digite o tipo do imovel (Casa, Apartamento ou Comercial):'))
+
+consumo_mensal = float(input('Digite o consumo mensal em metros cubicos:'))
+
+match imovel:
+    case 'comercial':
+        print('Tarifa comercial aplicada, consulte o plano corporativo.')
+
+    case 'apartamento':
+        if consumo_mensal<10:
+            print('Consum economico - excelente controle de agua')
+        elif consumo_mensal >=10 and consumo_mensal<=25:
+            print('Consumo moderado - dentro do padrao residencial')
+        else:
+            print('consumo excessivo - adote medidas de economiaa e verifique vazamentos.')
+
+    case 'casa':
+        #nao coloquei ate 10 pq nas instruçoes diziam o consumo ate 10 pro apartamento
+        #as outras duas opçoes se repetiram nas duas intrucoes ent coloquei, com a diferença que  no ap eh limitado entre 10 e 25 por causa do consum economico de 10 que ele tem a mais
+        if consumo_mensal<=25:
+            print('Consumo moderado - dentro do padrao residencial')
+        else:
+            print('consumo excessivo - adote medidas de economiaa e verifique vazamentos.')
+
+    case _:
+        print('opçao de imovel invalida')
